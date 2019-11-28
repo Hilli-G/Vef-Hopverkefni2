1f6a34647441dd03b5f033987e1b1960d1c0d70a
elas99 <elas99@hotmail.com>
2019-11-28 20:48:32 +0000

    bæti við media í css

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import List from './lib/list.js';
-import { el } from './lib/helpers.js';
+import { el, empty } from './lib/helpers.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const page = document.querySelector('body');
-  const isLecturePage = page.classList.contains('lecture-page');
-  const mainContainer = document.getElementById('content');
-  fetchJSONFile('lectures.json', (data) => {
-    console.log(data);
-    const listContainer = document.querySelector('ul');
-    Object.keys(data.lectures).forEach((key) => {
-      const element = el('li', el('div', el('img'), el('div',data.lectures[key].category), el('div', data.lectures[key].slug)));
-      if (data.lectures[key].hasOwnProperty('thumbnail')) {
-        element.querySelector('img').setAttribute('src', data.lectures[key].thumbnail);
-      }
-      listContainer.appendChild(element);
-    });
-  });
-  if (isLecturePage) {
-
-  } else {
-    const list = new List();
-    list.load();
-  }
-});
 
 function fetchJSONFile(path, callback) {
   const httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function () {
+  httpRequest.onreadystatechange = function request() {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
         const data = JSON.parse(httpRequest.responseText);
@@ -38,14 +16,82 @@ function fetchJSONFile(path, callback) {
   httpRequest.send();
 }
 
-function displayJSONData(data) {
-  for (const item in data) {
-    console.log(item);
+document.addEventListener('DOMContentLoaded', () => {
+  const page = document.querySelector('body');
+  const isLecturePage = page.classList.contains('lecture-page');
+  const buttonContainer = document.getElementById('buttons');
+  const mainContainer = document.getElementById('content');
+  const listContainer = document.querySelector('ul');
+  empty(listContainer);
+  displayJSONData();
+  if (isLecturePage) {
+
+  } else {
+    const list = new List();
+    list.load();
   }
+});
+document.addEventListener('click', (e) => {
+  const listContainer = document.querySelector('ul');
+  if (e.target && e.target.id === 'css') {
+    empty(listContainer);
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      displayJSONData('css');
+    } else {
+      displayJSONData();
+    }
+  }
+  if (e.target && e.target.id === 'html') {
+    empty(listContainer);
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      displayJSONData('css');
+    } else {
+      displayJSONData();
+    }
+  }
+  if (e.target && e.target.id === 'javascript') {
+    empty(listContainer);
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      displayJSONData('css');
+    } else {
+      displayJSONData();
+    }
+  }
+});
+
+
+function displayJSONData(type) {
+  const page = document.querySelector('body');
+  const isLecturePage = page.classList.contains('lecture-page');
+  const buttonContainer = document.getElementById('buttons');
+  const mainContainer = document.getElementById('content');
+  const listContainer = document.querySelector('ul');
+  fetchJSONFile('lectures.json', (data) => {
+    Object.keys(data.lectures).forEach((key) => {
+      const buttons = el('button', data.lectures[key].category);
+      buttons.setAttribute('id', data.lectures[key].category);
+      const element = el('li', el('div', el('img'), el('p', (data.lectures[key].category).toUpperCase()), el('h1', data.lectures[key].title)));
+      if (Object.prototype.hasOwnProperty.call(data.lectures[key], 'thumbnail')) {
+        element.querySelector('img').setAttribute('src', data.lectures[key].thumbnail);
+      }
+      if (!document.getElementById(data.lectures[key].category)) {
+        buttonContainer.appendChild(buttons);
+      }
+      if (type === 'css' && (data.lectures[key].category === 'css')) {
+        listContainer.appendChild(element);
+      } else if (type === 'html' && (data.lectures[key].category === 'html')) {
+        listContainer.appendChild(element);
+      } else if (type === 'javascript' && (data.lectures[key].category === 'javascript')) {
+        listContainer.appendChild(element);
+      } else if (type !== 'css' && type !== 'html' && type !== 'javascript') {
+        listContainer.appendChild(element);
+      }
+    });
+  });
 }
 
 
-
-
 // SVÆÐI FYRIR FYRIRLESTUR.HTML
-
